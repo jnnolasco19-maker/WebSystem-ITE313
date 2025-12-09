6c99bd7bcb9ed0c53ecc219cd8de37dd3eaafa6a
jnnolasco19-maker <jnnolasco19@gmail.com>
2025-12-09 09:02:40 +0800

    Added Bootstrap template and Home controller

---
+++ app/Views/template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ITE311 - CI4</title>
+
+    <!-- BOOTSTRAP CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ITE311</a>
+  </div>
+</nav>
+
+<div class="container mt-5">
+    <h1 class="text-center">Welcome to CodeIgniter 4 with Bootstrap!</h1>
+</div>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
